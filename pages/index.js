f1d01ea5e6807aa7553accea3f23941e54db92d5
Laith Harb <harblaith77@gmail.com>
2020-07-18 18:43:24 -0400

    Added polaris to app

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
-const Index = () => (
-    <div>
-        <p>Sample app using React and Next.js</p>
-    </div>
-)
+import {EmptyState, Layout, Page} from '@shopify/polaris'
+
+function Index() {
+    return (
+        <Page>
+            <Layout>
+                <EmptyState
+                    heading="Manage your inventory transfers"
+                    action={{content: 'Select Products'}}
+                    secondaryAction={{content: 'Learn more', url: 'https://help.shopify.com'}}
+                    image="https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
+                >
+                    <p>
+                        Select Products
+                    </p>
+                </EmptyState>
+            </Layout>
+        </Page>
+    )
+}
 
 export default Index
